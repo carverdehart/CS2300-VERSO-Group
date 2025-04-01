18ca51e613eff914af8beda815a0601b2ad3b61e
Carver DeHart <143362988+carverdehart@users.noreply.github.com>
2025-04-01 10:29:19 -0400

    Update datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,9 +5,13 @@
 
 int main()
 {
+    //creating string containing set date and time
     std::string date_str = "2022-03-17 10:45:30";
+    //creating date_obj
     std::tm date_obj = {};
+    //Allows us to treat date_str as an input stream
     std::istringstream ss(date_str);
+    //inputs the time and stores it
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
